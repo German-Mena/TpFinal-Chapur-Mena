9eb0ff11095860214024daef3ee497f467201aa6
julietachapur <juli.chapur@hotmail.com>
2022-07-15 10:30:12 -0300

    agregados

---
+++ negocio/TipoPropiedadNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class tipoPropiedadNegocio
+    {
+        public List<tipoPropiedad> listar()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<tipoPropiedad> lista = new List<tipoPropiedad>();
+            try
+            {
+
+                datos.setearConsulta(Diccionario.LISTAR_TIPOS_PROPIEDADES);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    tipoPropiedad aux = new tipoPropiedad();
+                    aux.id = (int)datos.Lector["id"];
+                    aux.descripcion = (string)datos.Lector["descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ negocio/tipoContratoNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class tipoContratoNegocio
+    {
+        public List<tipoContrato> listar()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<tipoContrato> lista = new List<tipoContrato>();
+            try
+            {
+
+                datos.setearConsulta(Diccionario.LISTAR_TIPOS_CONTRATOS);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    tipoContrato aux = new tipoContrato();
+                    aux.id = (int)datos.Lector["id"];
+                    aux.descripcion = (string)datos.Lector["descripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
