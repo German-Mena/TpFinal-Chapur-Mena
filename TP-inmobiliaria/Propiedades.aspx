51b53ca445518cfc82cf2f03b621687b7ca2e94a
julietachapur <juli.chapur@hotmail.com>
2022-07-15 14:09:56 -0300

    Detalle propiedad terminado el filtro

--- TP-inmobiliaria/Propiedades.aspx
+++ TP-inmobiliaria/Propiedades.aspx
@@ -74,12 +74,11 @@
 <!DOCTYPE html>
     <html xmlns="http://www.w3.org/1999/xhtml">
     <body>
-    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+<%--    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>--%>
 
     <hr />
         <asp:UpdatePanel runat="server">
             <ContentTemplate>
-                
                 <div class="row">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
                         <div
--- TP-inmobiliaria/Propiedades.aspx.cs
+++ TP-inmobiliaria/Propiedades.aspx.cs
@@ -38,35 +38,7 @@ namespace TP_inmobiliaria
             {
                 Session.Add("error", ex);
             }
-            
-
-            //protected void btn_verpropiedad_click(object sender, eventargs e)
-            //{
-            //    int idpropiedad = 1;
-            //    response.redirect("detallepropiedad.aspx?idpropiedad=" + idpropiedad, false);
-            //    //para un futuro: tener en cuenta que esta pasando debajo de esta linea
-            //}
-            //protected void ddlTipoContrato_SelectedIndexChanged(object sender, EventArgs e)
-            //{
-            //    if (IsPostBack)
-            //    {
-            //        int id = int.Parse(ddlTipoContrato.SelectedItem.Value);
-            //        ddlTipoContrato.DataSource = ((List<tipoContrato>)Session["listaPropiedades"]).FindAll(x => x.id == id);
-            //        ddlTipoContrato.DataBind();
-            //    }
-            //}
         }
-
-        protected void ddlTipoPropiedad_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (IsPostBack)
-            {
-                int id = int.Parse(ddlTipoPropiedad.SelectedItem.Value);
-                ddlTipoPropiedad.DataSource = ((List<tipoPropiedad>)Session["listaTipos"]).FindAll(x => x.id == id);
-                ddlTipoPropiedad.DataBind();
-            }
-        }
-
         protected void btnCargar_Click(object sender, EventArgs e)
         {
             string idPropiedad = ddlTipoPropiedad.Text;
@@ -74,9 +46,7 @@ namespace TP_inmobiliaria
             propiedadNegocio propiedad = new propiedadNegocio();
             listaPropiedades = propiedad.listarPropiedades_cards(idPropiedad,idContrato);
             Session.Add("listaPropiedades", listaPropiedades);
-            llamarDesplegable();
         }
-
         protected void llamarDesplegable()
         {   //conseguimos para el desplegable de tipoPropiedad
             tipoPropiedadNegocio tipoProp = new tipoPropiedadNegocio();
@@ -102,6 +72,16 @@ namespace TP_inmobiliaria
             {
                 int id = int.Parse(ddlTipoContrato.SelectedItem.Value);
                 ddlTipoPropiedad.DataSource = ((List<tipoContrato>)Session["listaContrato"]).FindAll(x => x.id == id);
+                ddlTipoPropiedad.DataBind();
+            }
+        }
+
+        protected void ddlTipoPropiedad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                int id = int.Parse(ddlTipoPropiedad.SelectedItem.Value);
+                ddlTipoPropiedad.DataSource = ((List<tipoPropiedad>)Session["listaTipos"]).FindAll(x => x.id == id);
                 ddlTipoPropiedad.DataBind();
             }
         }
