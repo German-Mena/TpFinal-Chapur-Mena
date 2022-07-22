41362b6ddbb624cf0681e6573a75aeb072b431e5
julietachapur <juli.chapur@hotmail.com>
2022-07-22 09:15:59 -0300

    nuevo archivo ubicacionNegocio

---
+++ negocio/ubicacionNegocio.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ubicacionNegocio
+    {
+        public int BuscarID(string calle, string altura)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(Diccionario.LISTAR_UBICACION);
+                datos.setearParametro("@calle", calle);
+                datos.setearParametro("@altura", altura);
+
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    int id = (int)datos.Lector["id"];
+                    return id;
+                }
+
+                return 0;
+            }  
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        public void Agregar(ubicacion ubic)
+            {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(Diccionario.AGREGAR_UBICACION);
+                datos.setearParametro("@calle", ubic.calle);
+                datos.setearParametro("@altura", ubic.altura);
+                datos.setearParametro("@departamento", ubic.departamento);
+                datos.setearParametro("@ciudad", ubic.ciudad);
+                datos.setearParametro("@provincia", ubic.provincia);
+                datos.setearParametro("@pais", ubic.pais);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
